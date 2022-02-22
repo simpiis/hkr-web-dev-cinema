d2e47ecd983a09f9a34db5685b93e7c7db990b7f
simpiis <simon.ejderbrand@hotmail.com>
2022-02-22 16:34:21 +0100

    renamed index.js and updated some commands in the new js file
    index.js is now database.js.
    
    added comments to help make it easier to understand.
    
    sample select statement for all movies.
    
    get the seats for a specific showing

---
+++ database.js
@@ -0,0 +1,59 @@
+const betterSqlite3 = require('better-sqlite3');
+
+const db = betterSqlite3('./database/moviedb.db');
+
+// set directory to hkr-web-dev-cinema,
+//type node database.js in terminal to run code
+
+// select statement -- get everything about every movie
+let stmtSelectAllMovies = db.prepare(`
+SELECT * FROM movies
+`)
+
+let movies = stmtSelectAllMovies.all();
+
+console.log(movies);
+
+
+
+
+
+// get seat_list from a specific showing, can be used to print the booking section later
+
+let movieTitleTest = 'Warcraft'
+let startTimeTest = '2022-02-19 16:54:00'
+
+let stmt = db.prepare("SELECT seat_list FROM showings WHERE movie_title = '"+ movieTitleTest +"' AND start_time = '"+startTimeTest+"'");
+
+let seats = stmt.all();
+
+let seatInfo = seats[0].seat_list.split(",");
+
+
+console.log(seatInfo);
+
+
+/* DATABASE TABLES
+columns in showings table
+id --- auto incremented integer for primary key
+movie_title --- title of the movie
+room_name --- name of room the movie is shown in
+start_time --- when the movie starts in the format of YYYY-MM-DD HH:MM:SS
+end_time --- when the movie ends in the format of YYYY-MM-DD HH:MM:SS
+booked_seats_counter --- counts the number of booked seats ie. number of "t" in seat_list
+seat_list --- list of all seats in format 1f,2f,3f etc...  f for false meaning not booked, insert t for true if booked
+
+
+columns in movies table
+title --- title of the movie
+length --- length of movie in minutes
+link --- youtube link to the trailer
+synopsis --- synopsis of the movie, currently contains movie title + lorem ipsum
+rating --- age rating of movie in string form ie. R or PG-13
+
+
+columns in rooms table
+name --- name of the room
+seats --- amount of seats in the room, currently does not hold any relevant information
+
+*/
