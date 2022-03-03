02649ddbb1048d788e2d06f9ed613b7774dc3f4d
simpiis <simon.ejderbrand@hotmail.com>
2022-03-03 13:18:25 +0100

    form for signing in/up on account.html
    form to sign in using username + password is now visible. button still not connected to a working function

--- js/account.js
+++ js/account.js
@@ -1,9 +1,4 @@
-
-
-
-
 function accountMain() {
-  console.log("anders e cool")
 
   if (checkLogin()) {
     displayAccountInfo();
@@ -19,8 +14,11 @@ function accountMain() {
 // check of user is logged in
 function checkLogin() {
   if (window.sessionStorage.getItem("user")) {
+    console.log("user found")
     return true;
   }
+  console.log("user not logged in")
+  return false;
 }
 
 
@@ -30,16 +28,30 @@ function displayAccountInfo() {
 }
 
 function displayLogin() {
-  let body = document.getElementById("account-content");
+  let body = document.getElementById("login-form-div");
 
   body.innerHTML += `
-  <p> create or log in. Enter your credentials, if the account exists you will be logged in, otherwise we will create an account for you with the provided details</p> <br>
+  <p> Create an account or log in. Enter your credentials, if the account exists you will be logged in, otherwise we will create an account for you with the provided details</p>
+  <p> (this is unencrypted so please do not use real passwords) </p><br>
   <div id="sign-in>
   <label for="username-field" required> Username: </label> <br>
   <input type="text" id="username-field" name="username-field"> <br>
   <label for="password-field">Password: </label> <br>
-  <input type="password" id="password-field" name="password-field" required</input>
+  <input type="password" id="password-field" name="password-field" required</input> <br>
+  <button id="user-sign-in" type="button" onclick="verifySignIn()"> Sign in </button>
   </div>
   `
+}
+
+
+function verifySignIn() {
+  let username = document.getElementById("username-field").value;
+  let password = document.getElementById("password-field").value;
+
+  console.log("username: ",username)
+  console.log("password: ",password)
+
+  verifyLogin(username,password);
+
 }
 
