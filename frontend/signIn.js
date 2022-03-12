66b6a19e92b13f23b4c5cd7e7e41403cd50b3891
simpiis <simon.ejderbrand@hotmail.com>
2022-03-12 12:50:10 +0100

    signIn.js created and mostly functioning
    takes values from the sign in form and attempts to log in via api/login

---
+++ frontend/signIn.js
@@ -0,0 +1,58 @@
+async function startSignIn() {
+  document.getElementById('buttons').style.display = 'none';
+  document.getElementById('login-div').style.display = 'inline';
+  document.getElementById('login-div').innerHTML = createSignInForm();
+  
+  
+
+  document.getElementById('login-div').addEventListener('submit', async (event) => {
+    if (!event.target.closest('form[name="signIn"]')) { return };
+
+    event.preventDefault();
+
+    let formElements = document.forms.signIn.elements;
+    let reqBody = {};
+
+    for (let element of formElements) {
+      if (element.type === 'submit') { continue; }
+      reqBody[element.name] = element.value;
+    }
+
+    let result;
+    try {
+      result = await (await fetch('/api/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(reqBody)
+      })).json();
+    }
+    catch (ignore) { }
+
+    if (!result || result._error) {
+      alert("invalid credentials");
+      return;
+    } 
+
+    document.getElementById('login-div').style.display = 'none';
+
+    getLoggedIn();
+
+  });
+
+}
+
+
+function createSignInForm() {
+  return `
+    <h1>Sign in</h1>
+    <form name="signIn">
+      <label>
+        <span>Username:</span><input required name="username" type="text">
+      </label>
+      <label>
+        <span>Password:</span><input required name="password" type="password">
+      </label>
+      <input type="submit" value="Sign In">
+    </form>
+  `;
+}
