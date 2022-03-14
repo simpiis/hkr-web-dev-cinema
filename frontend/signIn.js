3215852068f3217dd9d9ec45f82f0a9c13a2e103
simpiis <simon.ejderbrand@hotmail.com>
2022-03-14 20:59:13 +0100

    event listener in signin.js applied multiple times
    moved add event listener to a separate function. function is now called in main.js once as part of the initialization

--- frontend/signIn.js
+++ frontend/signIn.js
@@ -1,9 +1,28 @@
 async function startSignIn() {
   document.getElementById('buttons').style.display = 'none';
-  document.getElementById('login-div').style.display = 'inline';
   document.getElementById('login-div').innerHTML = createSignInForm();
-  
-  
+  document.getElementById('login-div').style.display = 'inline';
+
+}
+
+
+function createSignInForm() {
+  return `
+    <h1>Sign in</h1>
+    <form name="signIn">
+      <label>
+        <span>Username:</span><input required name="username" type="text">
+      </label>
+      <label>
+        <span>Password:</span><input required name="password" type="password">
+      </label>
+      <input type="submit" value="Sign In">
+    </form>
+  `;
+}
+
+
+function assignSignIn() {
 
   document.getElementById('login-div').addEventListener('submit', async (event) => {
     if (!event.target.closest('form[name="signIn"]')) { return };
@@ -31,7 +50,7 @@ async function startSignIn() {
     if (!result || result._error) {
       alert("invalid credentials");
       return;
-    } 
+    }
 
     document.getElementById('login-div').style.display = 'none';
 
@@ -39,20 +58,5 @@ async function startSignIn() {
 
   });
 
-}
 
-
-function createSignInForm() {
-  return `
-    <h1>Sign in</h1>
-    <form name="signIn">
-      <label>
-        <span>Username:</span><input required name="username" type="text">
-      </label>
-      <label>
-        <span>Password:</span><input required name="password" type="password">
-      </label>
-      <input type="submit" value="Sign In">
-    </form>
-  `;
 }
