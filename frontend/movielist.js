cbdb08731caee13af876cabcee099af1d4fc288a
Bujar <bujarskola@gmail.com>
2022-03-20 12:44:06 +0100

    filter by age/rating

--- frontend/movielist.js
+++ frontend/movielist.js
@@ -1,23 +1,48 @@
 const movies = [];
 
 function makeTable() {
+    resetTable();
+    const x = document.getElementById("mySelect").value;
     loadMov();
 
     let table = document.querySelector(".moviestable");
 
-
     for (const obj of movies) {
-        const tr = table.insertRow();
-        tr.insertCell().textContent = obj.title;
-        tr.insertCell().textContent = obj.length;
-        tr.insertCell().textContent = obj.rating;
-        tr.insertCell().textContent = obj.start_time;
+        if (x == 'all') {
+            const tr = table.insertRow();
+            tr.insertCell().textContent = obj.title;
+            tr.insertCell().textContent = obj.length;
+            tr.insertCell().textContent = obj.rating;
+            tr.insertCell().textContent = obj.start_time;
+        }
+        if (x == 'pg-13' && obj.rating == 'PG-13') {
+            const tr = table.insertRow();
+            tr.insertCell().textContent = obj.title;
+            tr.insertCell().textContent = obj.length;
+            tr.insertCell().textContent = obj.rating;
+            tr.insertCell().textContent = obj.start_time;
+        }
+
+        if (x == 'r' && obj.rating == 'R') {
+            const tr = table.insertRow();
+            tr.insertCell().textContent = obj.title;
+            tr.insertCell().textContent = obj.length;
+            tr.insertCell().textContent = obj.rating;
+            tr.insertCell().textContent = obj.start_time;
+        }
     }
 
 }
 
-function sortByRating() {
-    alert("test");
+
+function resetTable() {
+    for (const tr of document.querySelectorAll(".moviestable tr")) {
+        for (const child of[...tr.children]) {
+            if (child.tagName !== "TH") {
+                child.remove();
+            }
+        }
+    }
 }
 
 
