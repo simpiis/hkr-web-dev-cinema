9728db4f1a919c9d90922116b5635493d7e3bbdc
Bujar <bujarskola@gmail.com>
2022-03-20 10:38:31 +0100

    style for table

--- frontend/movielist.js
+++ frontend/movielist.js
@@ -1,30 +1,34 @@
 const movies = [];
 
 function makeTable() {
-  loadMov();
+    loadMov();
 
-  let table = document.querySelector(".moviestable");
+    let table = document.querySelector(".moviestable");
 
 
-  for (const obj of movies) {
-    const tr = table.insertRow();
-    tr.insertCell().textContent = obj.title;
-    tr.insertCell().textContent = obj.length;
-    tr.insertCell().textContent = obj.rating;
-  }
+    for (const obj of movies) {
+        const tr = table.insertRow();
+        tr.insertCell().textContent = obj.title;
+        tr.insertCell().textContent = obj.length;
+        tr.insertCell().textContent = obj.rating;
+    }
 
+}
+
+function sortByRating() {
+    alert("test");
 }
 
 
 async function loadMov() {
-  let url = 'http://localhost:3000/api/movies';
+    let url = 'http://localhost:3000/api/movies';
 
-  const response = await fetch(url);
-  const data = await response.json();
-  return data;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
 }
 
-(async () => {
-  const arr = await loadMov();
-  movies.push(...arr);
+(async() => {
+    const arr = await loadMov();
+    movies.push(...arr);
 })();
