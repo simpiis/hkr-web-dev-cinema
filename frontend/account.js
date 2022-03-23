9933a6c93fb6dd47f69081a588e729b4073ecc8d
Besnik <besnikskola@gmail.com>
2022-03-23 23:45:55 +0100

    getIsLoggedIn to see if user is signed in, can only book movies when logged in otherwise alert error

--- frontend/account.js
+++ frontend/account.js
@@ -1,39 +1,46 @@
+let isLoggedIn = false;
+
+
 async function getLoggedIn() {
-  let user;
-  try {
-    user = await (await fetch('/api/login')).json();
-  }
-  catch (ignore) { }
+    let user;
+    try {
+        user = await (await fetch('/api/login')).json();
+    } catch (ignore) {}
 
-  if (!user || user._error) {
-    document.getElementById('buttons').style.display = 'inline';
+    if (!user || user._error) {
+        document.getElementById('buttons').style.display = 'inline';
 
-  } else {
-    renderHistory(user);
-    document.getElementById('buttons').style.display = 'none';
-    document.getElementById('booking-history-div').style.display = 'inline'
-    console.log(user);
-  }
+    } else {
+        renderHistory(user);
+        document.getElementById('buttons').style.display = 'none';
+        document.getElementById('booking-history-div').style.display = 'inline'
+        isLoggedIn = true;
+        console.log(user);
+    }
+}
+
+function getIsLoggedIn() {
+    return isLoggedIn;
 }
 
 
 async function renderHistory(user) {
-  let bookingHistory;
-  let html;
-  let doc = document.getElementById('booking-history-div');
+    let bookingHistory;
+    let html;
+    let doc = document.getElementById('booking-history-div');
 
-  try {
-    bookingHistory = await (await fetch('/api/bookings/' + user.username)).json();
-  } catch (ignore) { }
+    try {
+        bookingHistory = await (await fetch('/api/bookings/' + user.username)).json();
+    } catch (ignore) {}
 
-  console.log(user.username);
-  console.log(bookingHistory);
+    console.log(user.username);
+    console.log(bookingHistory);
 
-  for (let item of bookingHistory) {
-    html += `
+    for (let item of bookingHistory) {
+        html += `
     <p> ${item.movie_title}  ${item.start_time}  ${item.end_time}  ${item.room_name}   </p>
     `
-  }
-  doc.innerHTML = html;
+    }
+    doc.innerHTML = html;
 
 }
