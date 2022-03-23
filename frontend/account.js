180cdb1e6072a183daa3dc966ff250e879efd3dc
simpiis <simon.ejderbrand@hotmail.com>
2022-03-23 21:12:32 +0100

    now prints booking history to page in accont.js

--- frontend/account.js
+++ frontend/account.js
@@ -12,27 +12,29 @@ async function getLoggedIn() {
     renderHistory(user);
     document.getElementById('buttons').style.display = 'none';
     document.getElementById('booking-history-div').style.display = 'inline'
-    console.log(user);
+    
   }
 }
 
 
 async function renderHistory(user) {
   let bookingHistory;
-  let html;
+  let html = "";
   let doc = document.getElementById('booking-history-div');
 
   try {
     bookingHistory = await (await fetch('/api/bookings/' + user.username)).json();
   } catch (ignore) { }
   
-  console.log(user.username);
+  console.log("booking history: ");
   console.log(bookingHistory);
+  let counter = 0;
 
   for (let item of bookingHistory) {
     html += `
-    <p> ${item.movie_title}  ${item.start_time}  ${item.end_time}  ${item.room_name}   </p>
+    <p> ${counter++}                           ${item.movie_title}  ${item.start_time}  ${item.end_time}  ${item.room_name}   </p>
     `
+    
   }
   doc.innerHTML = html;
 
