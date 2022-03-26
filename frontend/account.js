963b34ea7779c3de1d544d2ecbb0213c83c03586
simpiis <simon.ejderbrand@hotmail.com>
2022-03-26 16:13:56 +0100

    bookings in account history now separated into future and old
    2 arrays, oldBookings and futureBookings. They hold bookings based on the current date.  future dates are displayed first

--- frontend/account.js
+++ frontend/account.js
@@ -35,13 +35,38 @@ async function renderHistory(user) {
 
   console.log(user.username);
   console.log(bookingHistory);
-  let counter = 0;
+  let oldBookings = [];
+  let futureBookings = [];
 
   for (let item of bookingHistory) {
-    html += `
-    <p> ${counter++}                           ${item.movie_title}  ${item.start_time}  ${item.end_time}  ${item.room_name}   </p>
+    if (item.start_time > new Date().toLocaleString) {
+      futureBookings[futureBookings.length] = item;
+    } else {
+      oldBookings[oldBookings.length] = item;
+    }
+  }
+ 
+  
+  if (futureBookings.length >= 1) {
+    html += "<h1>Upcoming bookings </h1>"
+  
+
+    for (let item of futureBookings) {
+      html += `
+    <p> ${item.movie_title} | ${item.start_time} | ${item.end_time} | ${item.room_name}   </p>
     `
     
+    }
+  }
+
+  if (oldBookings.length >= 1) {
+    html += "<h1>Past bookings </h1>"
+  
+    for (let item of oldBookings) {
+      html += `
+    <p> ${item.movie_title} | ${item.start_time} | ${item.end_time} | ${item.room_name}   </p>
+    `
+    }
   }
   doc.innerHTML = html;
 
